3a0784aef8f926f6f689d4228d285bdc02243945
briws <briws@pm.me>
2021-04-30 22:07:13 +0300

    05 - sort with destruction

--- js05/main.js
+++ js05/main.js
@@ -5,11 +5,12 @@ const array = [777, 23, 12, 43, 32, 65, 82, 21, 0, 99]
 for (let j = 0; j < array.length - 1; j++) {
   for (let i = 0; i < array.length - 1 - j; i++) {
     if (array[i] > array[i + 1]) {
-      let temp = array[i + 1]
+      [array[i], array[i + 1]] = [array[i + 1], array[i]]
+      /*let temp = array[i + 1]
       array[i + 1] = array[i]
-      array[i] = temp
+      array[i] = temp*/
     }
   }
 }
-
 console.log(array)
+
