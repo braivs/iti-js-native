811ca1b308fd12a11f72141e9e62011ed3b3ef48
briws <briws@pm.me>
2021-04-30 21:56:43 +0300

    05 - по возрастанию вариант 1

---
+++ js05/main.js
@@ -0,0 +1,15 @@
+const array = [777, 23, 12, 43, 32, 65, 82, 21, 0, 99]
+
+
+// по возрастанию
+for (let j = 0; j < array.length - 1; j++) {
+  for (let i = 0; i < array.length - 1 - j; i++) {
+    if (array[i] > array[i + 1]) {
+      let temp = array[i + 1]
+      array[i + 1] = array[i]
+      array[i] = temp
+    }
+  }
+}
+
+console.log(array)
